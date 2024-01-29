47224a19bcc8101145f1d1a1db8cf897ab761e13
Adnan <adnanshaikh11501@gmail.com>
2024-01-29 18:46:04 +0530

    INTERNAL, Provider.tf & Mutable app code

---
+++ mutable-infra/env-prod/prod-backend.tfvars
@@ -0,0 +1,6 @@
+    bucket  = "adnan-tf-state-bucket"
+    key     = "mutable-infra/cart/prod/terraform.tfstate"
+    region  = "us-east-1"
+
+
+    
---
+++ mutable-infra/variables.tf
@@ -0,0 +1,10 @@
+variable "COMPONENT" {}
+variable "ENV" {}
+variable "APP_VERSION" {}
+variable "APP_PORT" {}
+variable "INTERNAL" {}
+
+variable "SPOT_INSTANCE" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "OD_INSTANCE" {}
+variable "OD_INSTANCE_COUNT" {}
